6e8687235873933abebede2452ab233d54444496
Aryan7691 <aryanjaiswal7march@gmail.com>
2025-02-19 19:54:43 +0530

    users/register endpoint completed

--- Backend/app.js
+++ Backend/app.js
@@ -4,6 +4,9 @@ const cors = require('cors');
 const express = require('express');
 const app = express();
 const connectToDb = require('./db/db');
+const userRoutes = require('./routes/user.routes');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 connectToDb();
 
@@ -12,5 +15,7 @@ app.use(cors());
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
+app.use('/users', userRoutes);
 
 module.exports = app;
