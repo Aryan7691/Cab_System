7612b5534b194db90c4f91ae726bc9bad7ec6950
Aryan7691 <aryanjaiswal7march@gmail.com>
2025-03-08 17:39:43 +0530

    captain routes completed

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -10,6 +10,12 @@ module.exports.registerUser = async (req, res, next) => {
     return res.status(400).json({ errors: errors.array() });
   }
   const { email, fullname, password } = req.body;
+  const isUserAlreadyExists = await userModel.findOne({ email });
+
+  if (isUserAlreadyExists) {
+    return res.status(400).json({ message: 'User already exists' });
+  }
+  
   const hashedPassword = await userModel.hashPassword(password);
   const user = await userService.createUser({ email, firstname: fullname.firstname, lastname: fullname.lastname, password: hashedPassword });
 
